4e2ca9687530c8e20b1f284aed257906e1000cdc
Elyamira <itiselya@gmail.com>
2022-11-13 17:26:56 +0000

    Removes counter

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
-import Home from './Pages/Home';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        {/* <Counter /> */}
-        <Home />
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <Home />
+            </header>
+        </div>
+    );
 }
 
 export default App;
