693b19b6de9f30d6c3e52574c6b3c532a8c76674
Elyamira <itiselya@gmail.com>
2022-11-13 17:27:21 +0000

    Adds routes

--- front/src/index.js
+++ front/src/index.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux';
 import { Auth0Provider } from "@auth0/auth0-react";
 import history from "./utils/history";
 import { getConfig } from "./config";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Favs from './routes/Favs';
 
 const onRedirectCallback = (appState) => {
   history.push(
@@ -27,9 +29,13 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Auth0Provider {...providerConfig}>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}></Route>
+            <Route path="/favs" element={<Favs />}></Route>
+          </Routes>
+        </BrowserRouter>
       </Auth0Provider>
-
     </Provider>
 
   </React.StrictMode>,
