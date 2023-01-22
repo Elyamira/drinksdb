4d9459ca5f2756316e30d8644ef17ee31eafff78
Elyamira <itiselya@gmail.com>
2023-01-22 12:57:46 +0000

    Prettier updates

--- front/src/components/Search.js
+++ front/src/components/Search.js
@@ -1,26 +1,33 @@
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { filter } from './slices/drinksSlice';
 const Search = (props) => {
-    const [searchedItem, setSearchedItem] = useState("");
+    const [searchedItem, setSearchedItem] = useState('');
     const dispatch = useDispatch();
 
-
     const handleChange = (e) => {
-        setSearchedItem(e.target.value)
-    }
+        setSearchedItem(e.target.value);
+    };
     const submitSearchForm = (e) => {
-        e.preventDefault()
-        props.onGetValue(searchedItem)
-        setSearchedItem("")
-        dispatch(filter(searchedItem))
-
-    }
+        e.preventDefault();
+        props.onGetValue(searchedItem);
+        setSearchedItem('');
+        dispatch(filter(searchedItem));
+    };
     return (
         <form onSubmit={submitSearchForm}>
             <label htmlFor='searchBar'></label>
-            <input id="searchBar" className='border border-sky-500' onChange={handleChange} value={searchedItem} data-testid="home-search-input"></input>
-            <button type='submit' data-testid="home-search-button">Search!</button>
-        </form>)
-}
-export default Search
+            <input
+                id='searchBar'
+                className='border border-sky-500'
+                onChange={handleChange}
+                value={searchedItem}
+                data-testid='home-search-input'></input>
+            <button type='submit' data-testid='home-search-button'>
+                Search!
+            </button>
+        </form>
+    );
+};
+export default Search;
