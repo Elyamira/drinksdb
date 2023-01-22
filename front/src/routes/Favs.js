cc9599ebe013e2cfba3b8b8e4ab787246c43bdd5
Elyamira <itiselya@gmail.com>
2023-01-22 12:55:06 +0000

    Adds content to favourite page

--- front/src/routes/Favs.js
+++ front/src/routes/Favs.js
@@ -1,22 +1,35 @@
-import Navbar from '../components/navbar/Navbar';
-import Drinks from '../components/Drinks';
-import { selectAllDrinks } from '../components/slices/drinksSlice';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Favs = () => {
-    const allDrinks = useSelector(selectAllDrinks);
-    const {
-        user,
-        isAuthenticated,
-    } = useAuth0();
-    const drinksForThisUser = allDrinks.filter(drink => drink.creatorId === user.creatorId)
+    const { user } = useAuth0();
+    const allDrinks = useSelector((state) => state.drinksData);
 
-    return <div>
-        <Navbar />
-        <p>My favs</p>
-        <Drinks drinksFromDb={drinksForThisUser} />
+    let creatorId;
+    if (user) {
+        creatorId = user.sub;
+    }
+    const favouriteDrinks = allDrinks.drinks?.filter((drink) => {
+        return drink.isInFavourites.includes(creatorId);
+    });
 
-    </div>
-}
+    return (
+        <div>
+            <p>My favs</p>
+            {favouriteDrinks?.map((drink, index) => (
+                <div key={index} className='flex gap-5'>
+                    <p key={index}>{drink.name}</p>
+                    <button
+                        onClick={() => {
+                            console.log('remove');
+                        }}>
+                        Remove From favourites
+                    </button>
+                </div>
+            ))}
+            {/* <Drinks drinksFromDb={favouriteDrinks} /> */}
+        </div>
+    );
+};
 export default Favs;
