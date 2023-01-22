632460a505f3b490c90b26e31cdb772eae6df721
Elyamira <itiselya@gmail.com>
2023-01-22 12:46:59 +0000

    Creates page wrapper

---
+++ front/src/components/PageWrapper.js
@@ -0,0 +1,16 @@
+import Navbar from './navbar/Navbar';
+
+export const PageWrapper = ({ children }) => {
+    return (
+        <div>
+            <Navbar />
+            <div className='min-h-[calc(100vh-3rem-9rem)] pt-36'>
+                {children}
+            </div>
+
+            <div className='h-12 flex justify-center items-center border-t border-black'>
+                <p>From Elya with love</p>
+            </div>
+        </div>
+    );
+};
