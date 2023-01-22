89920b825ee0b407f54f0491f44357d451779959
Elyamira <itiselya@gmail.com>
2023-01-22 12:54:10 +0000

    Creates Error message component

---
+++ front/src/components/modal/ErrorMessage.js
@@ -0,0 +1,10 @@
+const ErrorMessage = () => {
+    return (
+        <div
+            className={`absolute min-w-max z-50 top-20 left-1/2 -translate-x-1/2 flex bg-red-600 rounded-xl py-1 px-6 items-center gap-2 justify-center`}>
+            <img src='/images/error-1.svg' className='w-6 h-6' alt='error' />
+            <p> Something went wrong</p>
+        </div>
+    );
+};
+export default ErrorMessage;
