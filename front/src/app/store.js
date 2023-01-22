ca1dcdf8432507e78cfb8ef460f524c048ad53ac
Elyamira <itiselya@gmail.com>
2023-01-22 12:48:42 +0000

    Prettier updates

--- front/src/app/store.js
+++ front/src/app/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import drinksReducer from "../components/slices/drinksSlice";
-import popupInputsReducerSlice from "../components/slices/popupInputsReducerSlice";
+import drinksReducer from '../components/slices/drinksSlice';
+import popupInputsReducerSlice from '../components/slices/popupInputsReducerSlice';
 import popupRandomDrinkSlice from '../components/slices/popupRandomDrinkSlice';
 import popupErrorMessageSlice from '../components/slices/popupErrorMessageSlice';
 
 export const store = configureStore({
-  reducer: {
-    drinksData: drinksReducer,
-    popupInputs: popupInputsReducerSlice,
-    popupRandomDrink: popupRandomDrinkSlice,
-    errorPopup: popupErrorMessageSlice,
-  },
+    reducer: {
+        drinksData: drinksReducer,
+        popupInputs: popupInputsReducerSlice,
+        popupRandomDrink: popupRandomDrinkSlice,
+        errorPopup: popupErrorMessageSlice,
+    },
 });
